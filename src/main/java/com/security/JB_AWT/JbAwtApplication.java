9ebe4a889f35e7d873eaa2e45d9a267cea79629c
Dk-underline <87069089+Dk-underline@users.noreply.github.com>
2023-01-18 00:08:59 +0530

    First Commit

---
+++ src/main/java/com/security/JB_AWT/JbAwtApplication.java
@@ -0,0 +1,13 @@
+package com.security.JB_AWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbAwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbAwtApplication.class, args);
+	}
+
+}
